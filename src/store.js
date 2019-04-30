832ce3d98313ce6597820b4fbaa5cc1a20fdd243
Sara Mayberry <saraamayberry@yahoo.com>
2019-04-29 22:06:52 -0600

    bookmark-server

---
+++ src/store.js
@@ -0,0 +1,16 @@
+const bookmarks = [{
+    id: 1,
+    title: "Test Bookmark",
+    url: "www.boookmark.test.com",
+    description: "This is a test bookmark",
+    rating: 5
+},
+{
+    id:2,
+    title: "Test Bookmark",
+    url: "www.boookmark.test.com",
+    description: "This is a test bookmark",
+    rating: 5
+}]
+
+module.exports = { bookmarks }
