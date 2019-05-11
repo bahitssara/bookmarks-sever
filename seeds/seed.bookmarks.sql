becb56a16ddd7b4ec68760212c80cfbe03e9b716
Sara Mayberry <saraamayberry@yahoo.com>
2019-05-10 20:14:54 -0600

    migration-seed

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,12 @@
+INSERT INTO bookmarks (title, body, weburl, rating)
+VALUES
+    ('Bookmark1', 'Test bookmark1', 'www.bookmark.com', '5'),
+    ('Bookmark2', 'Test bookmark2', 'www.bookmark.com', '4'),
+    ('Bookmark3', 'Test bookmark3', 'www.bookmark.com', '3'),
+    ('Bookmark4', 'Test bookmark4', 'www.bookmark.com', '2'),
+    ('Bookmark5', 'Test bookmark5', 'www.bookmark.com', '1'),
+    ('Bookmark1', 'Test bookmark6', 'www.bookmark.com', '5'),
+    ('Bookmark2', 'Test bookmark7', 'www.bookmark.com', '4'),
+    ('Bookmark3', 'Test bookmark8', 'www.bookmark.com', '3'),
+    ('Bookmark4', 'Test bookmark9', 'www.bookmark.com', '2'),
+    ('Bookmark5', 'Test bookmark10', 'www.bookmark.com', '1');
